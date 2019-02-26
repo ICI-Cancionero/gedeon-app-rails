2699faa691f558268f32712f4bb51e2d83b1bbf6
Luis Porras <lporras16@gmail.com>
2019-02-26 14:18:49 -0300

    Update songs.rb

--- app/admin/songs.rb
+++ app/admin/songs.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Song do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :content, :position
 #
 # or
 #

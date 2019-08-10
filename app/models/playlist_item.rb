dd757218dfa4db1489daa4193289f2bc6a05be59
Luis Porras <lporras16@gmail.com>
2019-08-10 12:14:02 -0400

    belongs_to optional

--- app/models/playlist_item.rb
+++ app/models/playlist_item.rb
@@ -1,6 +1,6 @@
 class PlaylistItem < ApplicationRecord
-  belongs_to :playlist_section
-  belongs_to :song
+  belongs_to :playlist_section, optional: true
+  belongs_to :song, optional: true
 
   default_scope { order(position: :asc) }
 end
--- app/models/playlist_section.rb
+++ app/models/playlist_section.rb
@@ -1,5 +1,5 @@
 class PlaylistSection < ApplicationRecord
-  belongs_to :playlist
+  belongs_to :playlist, optional: true
   has_many :playlist_items, -> { order(created_at: :asc) }, dependent: :destroy
   has_many :songs, -> { order(created_at: :asc) }, through: :playlist_items
 

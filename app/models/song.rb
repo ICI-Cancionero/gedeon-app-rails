b70e3d20317f71818cc6d725073122d37b607d42
Luis Porras <lporras16@gmail.com>
2020-11-08 07:19:07 -0300

    WIP adding audio to songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ApplicationRecord
   has_and_belongs_to_many :playlists
+  has_one_attached :karaoke_track
+  has_one_attached :acappella_track
+  has_one_attached :backing_track
+  has_one_attached :complete_track
 end

c8a3a1721f94b37ef5e1a0cd597091ad7e605cdc
Luis Alfredo Porras Páez <lporras16@gmail.com>
2022-06-29 14:03:50 -0500

    adding mailgun config

---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => ENV['APP_HOST'],
+  :authentication => :plain,
+}

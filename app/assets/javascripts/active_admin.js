f4ffe8ea3cc6df917564da67c0d284acdb43799b
Luis Porras <lporras16@gmail.com>
2024-01-01 12:27:16 -0500

    using revealjs to generate an slide for songs

---
+++ app/assets/javascripts/active_admin.js
@@ -0,0 +1,8 @@
+//= require active_admin/base
+//= require activeadmin_addons/all
+//= require stimulus
+//= require controllers
+
+const application = Stimulus.Application.start()
+
+application.register("hello", HelloController)
---
+++ app/assets/javascripts/revealjs.js
@@ -0,0 +1,1 @@
+//= require revealjs/reveal.js

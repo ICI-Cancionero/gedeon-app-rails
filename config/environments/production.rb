82c52c058d262ef62031df4aa198add7cc7af306
Luis Porras <lporras16@gmail.com>
2025-09-01 17:20:09 -0500

    assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.js_compressor = nil
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.css_compressor = nil
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
